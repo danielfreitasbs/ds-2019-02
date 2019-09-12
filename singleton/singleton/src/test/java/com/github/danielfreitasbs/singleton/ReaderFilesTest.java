6dc1a0ee62c93eb95181070e4d8bdbe36ea398d3
kyriosdata <fabio@inf.ufg.br>
2019-09-12 12:27:17 -0300

    assegura que não há como criar instancia senao via padrao

--- singleton/singleton/src/test/java/com/github/danielfreitasbs/singleton/ReaderFilesTest.java
+++ singleton/singleton/src/test/java/com/github/danielfreitasbs/singleton/ReaderFilesTest.java
@@ -1,9 +1,16 @@
 package com.github.danielfreitasbs.singleton;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class ReaderFilesTest {
+
+  @Test
+  void naoHaComoCriarInstanciaSenaoPorMeioDeGetInstance() {
+    assertThrows(IllegalAccessException.class,
+            () -> ReaderFiles.class.newInstance());
+  }
 
   @Test
   void instanciaCriada() throws InstantiationException, IllegalAccessException {
